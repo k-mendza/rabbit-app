928c04563ddd09dfce20ca91eaa08f1fb72d7d9a
Karol Mendza <karolmendza@gmail.com>
2019-11-06 21:48:09 +0100

    Initial commit

---
+++ src/main/java/ex/rabbit/RabbitAppApplication.java
@@ -0,0 +1,13 @@
+package ex.rabbit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitAppApplication.class, args);
+    }
+
+}
